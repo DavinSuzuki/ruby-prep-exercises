820297082ce3b8130899c351f4809164f50fbdb5
Davin Suzuki <davinkikaida@gmail.com>
2022-10-18 00:52:34 +0000

    Add exercises for arrays, hashes, and strings

---
+++ ruby_basics/hashes/label.rb
@@ -0,0 +1,10 @@
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
+numbers.each do |type, num|
+  puts "A #{type} number is #{num}"
+end 
+  
---
+++ ruby_basics/strings/string.rb
@@ -0,0 +1,2 @@
+empty_string1 = String.new 
+empty_string2 = ''
